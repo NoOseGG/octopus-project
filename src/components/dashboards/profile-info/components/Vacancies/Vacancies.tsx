c615484f3b0294007ae68258fd6f3bb05cfb9c21
NoOseGG <hoc751@gmail.com>
2023-12-11 21:52:49 +0300

    added sort in vacancies

--- src/components/dashboards/profile-info/components/Vacancies/Vacancies.tsx
+++ src/components/dashboards/profile-info/components/Vacancies/Vacancies.tsx
@@ -1,19 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useAppSelector } from '@app/hooks/reduxHooks';
 import MyVacancy from '@app/components/dashboards/profile-info/components/Vacancies/components/Vacancy/MyVacancy';
 import styled from 'styled-components';
 import CountVacancies from '@app/components/dashboards/profile-info/components/Vacancies/components/CountVacancies/CountVacancies';
+import { Vacancy } from '@app/store/types/Subject';
+import { Select } from 'antd';
+import {
+  PlaceholderText,
+  filterStyle,
+} from '@app/components/dashboards/dashboard/components/SearchFilters/styles/SearchFiltersStyles';
+
+enum Sort {
+  DATE,
+  NAME,
+}
+
+enum AscendingEnum {
+  ASCENDING = 'По возрастанию',
+  ASCENDING_REVERSE = 'По убыванию',
+}
 
 const Vacancies: React.FC = () => {
   const vacancies = useAppSelector((state) => state.searchProfile.profile.vacancy);
+  const [sortedVacancies, setSortedVacancies] = useState([...vacancies]);
+  const [ascending, setAscending] = useState(AscendingEnum.ASCENDING);
+
+  const sortVacancies = (): Vacancy[] => {
+    if (ascending === AscendingEnum.ASCENDING) {
+      setSortedVacancies([...vacancies]);
+      console.log(JSON.stringify(sortedVacancies));
+      return [...vacancies];
+    } else {
+      setSortedVacancies([...vacancies.reverse()]);
+      console.log(JSON.stringify(sortedVacancies));
+      return [...vacancies.reverse()];
+    }
+  };
+
+  useEffect(() => {
+    console.log(ascending);
+    sortVacancies();
+  }, [ascending]);
+
+  const data = [
+    {
+      value: AscendingEnum.ASCENDING,
+      label: 'По возрастанию',
+    },
+    {
+      value: AscendingEnum.ASCENDING_REVERSE,
+      label: 'По убыванию',
+    },
+  ];
 
   return (
     <>
-      {Boolean(vacancies.length) ? (
+      {Boolean(sortedVacancies.length) ? (
         <>
-          <CountVacancies count={vacancies.length} />
+          <Select
+            size="small"
+            showSearch
+            style={filterStyle}
+            placeholder={<PlaceholderText>{ascending}</PlaceholderText>}
+            optionFilterProp="children"
+            value={ascending}
+            onChange={setAscending}
+            filterOption={(input, option) => (option?.label ?? '').toLowerCase().includes(input.toLowerCase())}
+            options={data}
+          />
+          <CountVacancies count={sortedVacancies.length} />
           <VacanciesContainer>
-            {vacancies.map((item, index) => (
+            {sortedVacancies.map((item, index) => (
               <MyVacancy vacancy={item} key={index} />
             ))}
           </VacanciesContainer>

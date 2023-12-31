7c3af8024d14197f62e206c7f5287a34ee2b7220
NoOseGG <hoc751@gmail.com>
2023-12-31 17:25:59 +0300

    added word cloud in resumes

--- src/components/dashboards/profile-info/components/Resumes/Resumes.tsx
+++ src/components/dashboards/profile-info/components/Resumes/Resumes.tsx
@@ -3,12 +3,31 @@ import { useAppSelector } from '@app/hooks/reduxHooks';
 import styled from 'styled-components';
 import MyResume from '@app/components/dashboards/profile-info/components/Resumes/components/Resume/MyResume';
 import CountResumes from '@app/components/dashboards/profile-info/components/Resumes/components/CountResumes/CountResumes';
+import CloudTags from '@app/components/dashboards/profile-info/components/Vacancies/components/CloudTags/CloudTags';
 
 const Resumes: React.FC = () => {
   const resumes = useAppSelector((state) => state.searchProfile.profile.resume);
 
+  const words = resumes.reduce<string[]>((acc, obj) => {
+    const wordsArray = obj.skill_name?.split(';') ?? [];
+    return [...acc, ...wordsArray];
+  }, []);
+  const countObj: { [key: string]: number } = {};
+
+  // Подсчитываем количество каждого элемента в массиве
+  words.forEach((word) => {
+    countObj[word] = (countObj[word] || 0) + 1;
+  });
+
+  // Преобразуем объект в массив объектов
+  const keyWords: { name: string; value: number }[] = Object.entries(countObj).map(([name, value]) => ({
+    value,
+    name,
+  }));
+
   return (
     <>
+      {Boolean(keyWords.length) && <CloudTags keyWords={keyWords} />}
       {Boolean(resumes.length) ? (
         <>
           <CountResumes count={resumes.length} />

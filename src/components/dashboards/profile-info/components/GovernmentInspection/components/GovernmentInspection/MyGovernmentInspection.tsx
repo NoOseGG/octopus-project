c6b4eff4091a32f683586c56262d286ec4a61acc
NoOseGG <hoc751@gmail.com>
2024-02-28 00:01:49 +0300

    added status in government inspection

--- src/components/dashboards/profile-info/components/GovernmentInspection/components/GovernmentInspection/MyGovernmentInspection.tsx
+++ src/components/dashboards/profile-info/components/GovernmentInspection/components/GovernmentInspection/MyGovernmentInspection.tsx
@@ -27,8 +27,17 @@ const MyResume: React.FC<MyComponentProps> = ({ governmentInspection }) => {
   return (
     <VacancyContainer>
       <Title>
-        <TitleName>{governmentInspection.legal_entity_id_gov_name}</TitleName>{' '}
-        <TitleFromDttm>{`от ${formatDate(governmentInspection.from_dttm)}`}</TitleFromDttm>
+        <div>
+          <TitleName>{governmentInspection.legal_entity_id_gov_name}</TitleName>{' '}
+          <TitleFromDttm>{`от ${formatDate(governmentInspection.from_dttm)}`}</TitleFromDttm>
+        </div>
+        <div>
+          {governmentInspection.from_dttm && (
+            <TitleStatus color={getColor(governmentInspection.from_dttm)}>
+              {getTitle(governmentInspection.from_dttm)}
+            </TitleStatus>
+          )}
+        </div>
       </Title>
       <S.StyledTable>
         <tbody>
@@ -66,6 +75,10 @@ const VacancyContainer = styled.div`
 const Title = styled.div`
   width: 100%;
   font-size: 16px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 5px;
 `;
 
 const TitleName = styled.span`
@@ -75,3 +88,47 @@ const TitleName = styled.span`
 const TitleFromDttm = styled.span`
   color: grey;
 `;
+
+type TitleStatusProps = {
+  color: string;
+};
+
+const TitleStatus = styled.span<TitleStatusProps>`
+  color: #000;
+  margin-left: 2rem;
+  max-width: 9rem;
+  height: 1.125rem;
+  padding: 0.25rem 0.5rem;
+  border-radius: 6.25rem;
+  font-size: 0.5rem;
+  line-height: 0.725rem;
+  text-transform: uppercase;
+  letter-spacing: 0.05rem;
+  background-color: ${(props) => {
+    return props.color;
+  }};
+`;
+
+const getColor = (date: string): string => {
+  const currentDate = new Date().toISOString().slice(0, 10);
+  const valueDate = new Date(date).toISOString().slice(0, 10);
+  if (currentDate > valueDate) {
+    return '#64bb42';
+  } else if (valueDate > currentDate) {
+    return '#e770b0';
+  } else {
+    return '#ffd19b';
+  }
+};
+
+const getTitle = (date: string): string => {
+  const currentDate = new Date().toISOString().slice(0, 10);
+  const valueDate = new Date(date).toISOString().slice(0, 10);
+  if (currentDate > valueDate) {
+    return 'Завершена';
+  } else if (valueDate > currentDate) {
+    return 'Планируется';
+  } else {
+    return 'Проводится';
+  }
+};

12ff315d43f1d6f760848f43b6cc188e251c3414
NoOseGG <hoc751@gmail.com>
2024-03-16 12:20:42 +0300

    changed short name to full name in title profile info

--- src/components/dashboards/profile-info/components/GeneralInformation/TitleName/TitleName.tsx
+++ src/components/dashboards/profile-info/components/GeneralInformation/TitleName/TitleName.tsx
@@ -5,6 +5,7 @@ import { useAppSelector } from '@app/hooks/reduxHooks';
 const TitleName: React.FC = () => {
   const names = useAppSelector((state) => state.searchProfile.profile.names);
 
+  if (Boolean(names[0]?.full_name !== null)) return <Title>{names[0]?.full_name}</Title>;
   return names[0]?.short_name ? <Title>{names[0].short_name}</Title> : null;
 };
 

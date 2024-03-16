4292c9172445f34a91df371eadfe2dab514a4b53
NoOseGG <hoc751@gmail.com>
2024-03-17 02:05:37 +0300

    added token in interceptor

--- src/api/http.api.ts
+++ src/api/http.api.ts
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { AxiosError } from 'axios';
 import { ApiError } from '@app/api/ApiError';
 import { readToken } from '@app/services/localStorage.service';
+import { TOKEN_NAME } from '@app/constants/Constants';
 
 const BASE_URL = 'https://api.analytix.by/api/v1/auth/';
 
@@ -9,26 +10,11 @@ export const httpApi = axios.create({
   baseURL: BASE_URL,
 });
 
-httpApi.interceptors.request.use((config) => {
-  if (config.url === 'http://93.125.0.140:1338/api/v1/auth/login') {
-    const requestData = config.data;
-    console.log('Переданные данные в запросе:', requestData);
-    config.data = {
-      ...config.data,
-      additionalField1: 'value1',
-      additionalField2: 'value2',
-    };
+axios.interceptors.request.use((config) => {
+  const token = readToken();
+  if (token && config.headers) {
+    config.headers['Authorization'] = `${TOKEN_NAME} ${token}`;
   }
-
-  if (config.url === 'logout') {
-    console.log('LOGOUT ->!');
-    const token = readToken();
-    config.headers = {
-      ...config.headers,
-      Authorization: `Welcome ${token}`,
-    };
-  }
-
   return config;
 });
 

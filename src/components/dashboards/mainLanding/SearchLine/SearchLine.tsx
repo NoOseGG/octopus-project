96959aa7c3fa05f832fb67bee64e28a4aec86e5d
NoOseGG <hoc751@gmail.com>
2024-03-19 21:44:48 +0300

    added logics search in SearchLine

--- src/components/dashboards/mainLanding/SearchLine/SearchLine.tsx
+++ src/components/dashboards/mainLanding/SearchLine/SearchLine.tsx
@@ -1,14 +1,62 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { Container, InnerContainer } from '../styles/MainLandingStyles';
 import Search from 'antd/es/input/Search';
+import { useAppDispatch, useAppSelector } from '@app/hooks/reduxHooks';
+import _ from 'lodash';
+import { clearSearchData, doSearch } from '@app/store/slices/search/searchSlice';
+import { Dropdown } from 'antd';
+import DropdownMenu from '@app/components/dashboards/mainLanding/SearchLine/components/DropdownMenu/DropdownMenu';
 
 const SearchLine: React.FC = () => {
+  const dispatch = useAppDispatch();
+  const { results } = useAppSelector((state) => state.search.data);
+  const [query, setQuery] = useState('');
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    console.log(JSON.stringify(results));
+    if (Boolean(results.length)) setIsVisible(true);
+    else setIsVisible(false);
+  }, [results]);
+
+  const delaySearch = useCallback(
+    _.debounce((query: string) => {
+      dispatch(doSearch(query));
+    }, 500),
+    [dispatch],
+  );
+
+  const clearSearch = useCallback(
+    _.debounce(() => {
+      dispatch(clearSearchData());
+    }, 600),
+    [dispatch],
+  );
+
+  useEffect(() => {
+    if (query.trim().length > 2) {
+      delaySearch(query);
+    } else {
+      clearSearch();
+    }
+  }, [query]);
+
+  const handleClick = () => {
+    setIsVisible(false);
+  };
+
   return (
-    <Container backgroundColor={'#dbe1ea'}>
+    <Container backgroundColor={'#dbe1ea'} onClick={handleClick}>
       <InnerContainer>
         <SearchLineContainer>
-          <Search placeholder={'Введите УНП или название проверяемого субъекта'} enterButton={'Проверить'} />
+          <Dropdown overlay={<DropdownMenu data={results} />} visible={isVisible} placement={'bottom'}>
+            <Search
+              placeholder={'Введите УНП или название проверяемого субъекта'}
+              enterButton={'Проверить'}
+              onChange={(event) => setQuery(event.target.value)}
+            />
+          </Dropdown>
         </SearchLineContainer>
       </InnerContainer>
     </Container>
---
+++ src/components/dashboards/mainLanding/SearchLine/components/DropdownMenu/DropdownMenu.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import styled from 'styled-components';
+import { Organization } from '@app/store/slices/search/searchSlice';
+
+type MyComponentProps = {
+  data: Organization[];
+};
+
+const DropdownMenu: React.FC<MyComponentProps> = ({ data }) => {
+  return (
+    <DropdownMenuContainer>
+      {Boolean(data.length) && (
+        <>
+          {data.map((item, index) => (
+            <div key={index}>{item.full_name}</div>
+          ))}
+        </>
+      )}
+    </DropdownMenuContainer>
+  );
+};
+
+export default DropdownMenu;
+
+const DropdownMenuContainer = styled.div`
+  background-color: #fff;
+  padding: 10px;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 12px;
+  width: 100%;
+  height: 300px;
+  overflow-y: scroll;
+`;

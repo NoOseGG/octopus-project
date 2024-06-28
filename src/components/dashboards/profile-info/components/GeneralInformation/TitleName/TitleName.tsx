a967e138f2366a8508a08bfa76f4dd118707a126
NoOseGG <hoc751@gmail.com>
2024-06-28 13:22:35 +0300

    fixed favourite button in adaptive

--- src/components/dashboards/profile-info/components/GeneralInformation/TitleName/TitleName.tsx
+++ src/components/dashboards/profile-info/components/GeneralInformation/TitleName/TitleName.tsx
@@ -2,20 +2,33 @@ import React from 'react';
 import styled from 'styled-components';
 import { useAppSelector } from '@app/hooks/reduxHooks';
 import * as S from '@app/components/dashboards/profile-info/styles/ProfileInfoStyles';
+import Favourite from '@app/components/dashboards/profile-info/components/components/Buttons/Favourite/Favourite';
 
 const TitleName: React.FC = () => {
   const names = useAppSelector((state) => state.searchProfile.profile.names);
+  const unn = useAppSelector((state) => state.searchProfile.profile.unn);
 
   if (Boolean(names[0]?.full_name !== null))
     return (
       <>
         <Title>{names[0]?.full_name}</Title>
+        {unn && (
+          <FavouriteWrapper>
+            <Favourite unn={unn} />
+          </FavouriteWrapper>
+        )}
         <S.MyDivider />
       </>
     );
   return names[0]?.short_name ? (
     <TitleNameContainer>
       <Title>{names[0].short_name}</Title>
+      {unn && (
+        <FavouriteWrapper>
+          <Favourite unn={unn} />
+        </FavouriteWrapper>
+      )}
+      <S.MyDivider />
     </TitleNameContainer>
   ) : null;
 };
@@ -37,5 +50,15 @@ const Title = styled.h1`
 
   @media (max-width: 768px) {
     font-size: 16px;
+  }
+`;
+
+const FavouriteWrapper = styled.div`
+  margin-top: 10px;
+  width: 200px;
+  display: none;
+
+  @media (max-width: 1150px) {
+    display: block;
   }
 `;
